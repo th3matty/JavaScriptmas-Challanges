6ea9bc89c378135a923ef2d2f7ae0cf8bf5d3355
matthäus <ma_malek@protonmail.com>
2020-12-12 13:55:45 +0100

    add Day 12 challange

---
+++ Day 12/main.js
@@ -0,0 +1,59 @@
+function validTime(str) {
+    //  write code here.
+    let splitted = str.split(":")
+    const hour = parseInt(splitted[0])
+    const min = parseInt(splitted[1])    
+    
+    let result = hour <= 24 && min <= 60? true : false
+    return result
+   
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 #  :christmas_tree: JavaScriptmas-Challanges  :christmas_tree: 
 
-Every day from the frist december till christmas eve Scrimba provides the community with a coding challange which i am going to solve.
+[![Valid-Time.png](https://i.postimg.cc/X7dL3xCQ/Valid-Time.png)](https://postimg.cc/4m4VQ6w9)
 
+> *clone or visit directly this link with my solution:*
 
->For every Challange i will push a new branch.
-
->Each branch has a Readme file where you can see the challange.
->To see the solution head to "main.js" and to confirm it by jasmine you can easily open the html file in the browser.
-
->> *You can also follow the **Scrimba Link** which i will attach to the readme file for every challange. Copy paste my solution from "main.js" later on and hit the "RUN-Button".*
-
+> https://scrimba.com/scrim/co3cf4dd38f428bbbfb3906bb
